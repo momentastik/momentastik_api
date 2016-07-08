f9488bdbdfad94d6b56f189ca91dc18e39dd295d
Adrian Castellanos <adriancastellanos2c@gmail.com>
2016-07-08 11:27:26 +0200

    example

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+
 var http = require('http');
 var port = Number(process.env.PORT||1337);
 http.createServer(function (req, resp) {
 
-  resp.writeHead(200, {'Content-type':'text/html'})
-  resp.end('okeys');
+  if (req.method === 'GET'){
+      if (req.url == '/eventos'){
+        resp.writeHead(200, {'Content-type':'application/json'});
+        var ejemplo = {'api':'yEAH' };
+        resp.end(JSON.stringify(ejemplo));
 
+      }
+
+  }
 }).listen(port);
+console.log("Server is running at: "+ 1337);
