44d8f74a140abb49d1738507780bd04fbece7ea9
Adrian Castellanos <adriancastellanos2c@gmail.com>
2016-07-13 13:54:28 +0200

    I added some routes

--- controllers/event.js
+++ controllers/event.js
@@ -1,3 +1,16 @@
 var db = require('../core/db');
 var httpMsgs = require('../core/httpMsgs');
 var util = require('util');
+
+exports.getEvent = function(req, resp, id){
+
+	db.executeSQL("SELECT * FROM Place WHERE Place.id = "+id, function(error, rows){
+
+		if (error){
+			httpMsgs.show500(req, resp, err);
+		}else{
+			httpMsgs.sendJson(req, resp, rows);
+		}
+
+	});
+};
--- core/server.js
+++ core/server.js
@@ -3,41 +3,47 @@ var settings = require('../settings');
 var httpMsgs = require('./httpMsgs');
 var place = require('../controllers/place');
 var card = require('../controllers/card');
+var card = require('../controllers/event');
 
 
 http.createServer (function (req, resp){
 
 	switch (req.method){
 
-
 		case "GET":
 			switch(true){
-				case req.url=='/places':
 
-				place.getListPlaces(req, resp);
-				console.log("Writing places");
-					break;
-
+				/*CARDS*/
 				case req.url=='/card':
 					card.getListCards(req, resp);
-
 					break;
+
+				/*PLACES*/
+				case req.url=='/places':
+					place.getListPlaces(req, resp);
+					console.log("Writing places");
+					break;
+
 
 				case /places\/[0-9]+$/.test(req.url):
-					var id = /[0-9]+/.exec(req.url);//Get the ID
-					place.getPlace(req, resp,id);
-
+					var idPlace = /[0-9]+/.exec(req.url);//Get the ID of the place
+					place.getPlace(req, resp,idPlace);
 					break;
-				case /places\/[0-9]+\/events/.test(req.url):
+
+				case /places\/[0-9]+\/events$/.test(req.url):
 					var idPlace = /[0-9]+/.exec(req.url);//Get the ID of the place
 					place.getPlaceEvents(req, resp,idPlace);
+					break;
 
-						break;
+				/*EVENTS*/
+				case /events\/[0-9]+$/.test(req.url):
+					var idEvent = /[0-9]+/.exec(req.url);//Get the ID of the event
+					event.getEvent(req,resp,idEvent)
+					break;
 
 
 				default:
-
-				httpMsgs.show404(req, resp);
+					httpMsgs.show404(req, resp);
 			}
 
 		break;
