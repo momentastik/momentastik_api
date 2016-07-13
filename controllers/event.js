0bb76204b8cd6c4ddaeab5993b74c0c6c6ff34ac
Adrian Castellanos <adriancastellanos2c@gmail.com>
2016-07-13 13:59:31 +0200

    I added a route to get all the events

--- controllers/event.js
+++ controllers/event.js
@@ -2,6 +2,19 @@ var db = require('../core/db');
 var httpMsgs = require('../core/httpMsgs');
 var util = require('util');
 
+exports.getListEvents = function(req, resp){
+
+	db.executeSQL("SELECT * FROM Event", function(error, rows){
+
+		if (error){
+			httpMsgs.show500(req, resp, err);
+		}else{
+			httpMsgs.sendJson(req, resp, rows);
+		}
+
+	});
+};
+
 exports.getEvent = function(req, resp, id){
 
 	db.executeSQL("SELECT * FROM Event WHERE Event.id = "+id, function(error, rows){
