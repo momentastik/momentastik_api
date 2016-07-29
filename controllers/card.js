a9ce8e46dc1f678b5b2a19a00e8409b5aafecacd
Jaicomp <jaicompdeveloper@gmail.com>
2016-07-29 11:29:34 +0200

    error on cards

--- controllers/card.js
+++ controllers/card.js
@@ -7,7 +7,7 @@ exports.getListCards = function(req, resp){
 	db.executeSQL ("SELECT * FROM Card", function (error, rows){
 
 		if(error){
-			httpMsgs.show500(req, resp, err);
+			httpMsgs.show500(req, resp, error);
 		}else{
 			httpMsgs.sendJson(req, resp, rows);
 		}
