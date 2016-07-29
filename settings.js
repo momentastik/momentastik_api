c0ff4f9e24afebcfee64ab78f0913399ffe7cf34
Jaicomp <jaicompdeveloper@gmail.com>
2016-07-29 11:25:30 +0200

    first deploy on heroku

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 exports.dbConfig = {
-	host: '127.0.0.1',
-	user: 'bc5bf2d3332de3', //root
-	password: '5d0a407c',
-	database: 'momentastik'
+	host: '-cdbr-iron-east-04.cleardb.net',
+	user: 'b6c554615a1711', 
+	password: '26a49be9',
+	database: 'heroku_e2ac4ee7ad1fbad'
 };
 
 var webPortApp = process.env.PORT || 1337;
